a3e1bf23449962950226c9effefe8fed28825247
Angel Lim <brian.q.lim@gmail.com>
2019-02-12 15:59:31 -0800

    Created PhraseOMatic.java
    Program that demos arrays and rand

---
+++ 1 - Surface Level/PhraseOMatic/src/PhraseOMatic.java
@@ -0,0 +1,38 @@
+
+public class PhraseOMatic {
+
+	public static void main(String[] args) {
+		//makes three arrays of words
+		String[] wordListOne = {"24/7", "multi-Tier", "30,00 foot",
+				"B-to-B", "win-win", "front-end", "web-based", "pervasive",
+				"smart", "six-sigma", "critical-path", "dynamic"};
+		
+		String[] wordListTwo = {"empowered", "sticky", "value-added", "oriented",
+				"centric", "distirbuted", "clustered", "branded", "outside-the-box",
+				"positioned", "networked", "focused", "leveraged", "aligned", "targeted",
+				"shared", "cooperative", "accelerated"}; 
+		
+		String[] wordListThree = {"process", "tipping-point", "solution", "architecutre",
+				"core competency", "strategy", "mindshare", "portal", "space", "visions", 
+				"paradigm", "mission"};
+		
+		//finds out how many words are in each list
+		int oneLength = wordListOne.length;
+		int twoLength = wordListTwo.length;
+		int threeLength = wordListThree.length;
+		
+		//generate randomness
+		int rand1 = (int) (Math.random() * oneLength);
+		int rand2 = (int) (Math.random() * twoLength);
+		int rand3 = (int) (Math.random() * threeLength);
+		
+		//Build phrase variable
+		String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+		
+		//Print phrase
+		System.out.println("What we need is a " + phrase + "!!!");
+		
+		}//end of main
+
+	}//end of class PhraseOMatic
+
